4245bc9bbdbcb22458ee780c70fb00dd0fd6902a
Danilgopez1702 <80540485+Danilgopez1702@users.noreply.github.com>
2021-04-27 17:13:44 -0500

    agregar empleado

---
+++ sistema/agregar_empleado.php
@@ -0,0 +1,97 @@
+<?php
+include_once "header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picnic">
+    <center>
+    <div class="content">
+    <!-- Button to trigger modal -->
+<button class="btn btn-success btn-lg" data-toggle="modal" data-target="#modalForm">
+    Open Contact Form
+</button>
+        </div>
+</center>
+<!-- Modal -->
+<div class="modal fade" id="modalForm" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">
+                    <span aria-hidden="true">×</span>
+                    <span class="sr-only">Close</span>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">Contact Form</h4>
+            </div>
+            
+            <!-- Modal Body -->
+            <div class="modal-body">
+                <p class="statusMsg"></p>
+                <form role="form">
+                    <center>
+                   <div style="width: 200px" > <!-- this div just for demo display -->
+                <label class="dropimage miniprofile" style=" border-radius: 50%; padding-bottom: 75%; heigth:2px; width:145px;">
+                    <input name="image" title="Drop image or click me" type="file" id="imagen">
+                </label>
+             </div>
+                        </center>
+                    <div class="form-floating mb-3" >
+                        <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput"></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail">Apellido Paterno</label>
+                        <input type="text" class="form-control" id="inputEmail" placeholder="Enter your email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail">Apellido Materno</label>
+                        <input type="text" class="form-control" id="inputEmail" placeholder="Enter your email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail">Fecha de Ingreso</label>
+                        <input type="text" class="form-control" id="inputEmail" placeholder="Enter your email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail">Sueldo Mensual</label>
+                        <input type="text" class="form-control" id="inputEmail" placeholder="Enter your email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail">Departamento</label>
+                        <input type="text" class="form-control" id="inputEmail" placeholder="Enter your email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputMessage">Frecuencia de Pago</label>
+                        <textarea class="form-control" id="inputMessage" placeholder="Enter your message"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputMessage">Message</label>
+                        <textarea class="form-control" id="inputMessage" placeholder="Enter your message"></textarea>
+                    </div>
+                </form>
+            </div>
+            
+            <!-- Modal Footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary submitBtn" onclick="submitContactForm()">SUBMIT</button>
+            </div>
+        </div>
+    </div>
+</div>
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call(document.querySelectorAll('.dropimage'), function(img){
+    img.onchange = function(e){
+      var inputfile = this, reader = new FileReader();
+      reader.onloadend = function(){
+        inputfile.style['background-image'] = 'url('+reader.result+')';
+      }
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  });
+});
+    </script>    
+    </html>
+
