a19c7bbc491cead9c6e16f6e481e1a7da35a3e83
eed2e <eed.esteban@gmail.com>
2021-04-29 14:08:18 -0500

    .

---
+++ sistema/index.php
@@ -0,0 +1,127 @@
+<?php include_once "include/header.php"; ?>
+
+<!-- Begin Page Content -->
+<div class="content">
+
+	<!-- Page Heading -->
+	<div class="d-sm-flex align-items-center justify-content-between mb-4">
+		<h1 class="h3 mb-0 text-gray-800">Panel de Administración</h1>
+	</div>
+
+	<!-- Content Row -->
+	<div class="row">
+	
+		<!-- Earnings (Monthly) Card Example -->
+		<a class="col-xl-3 col-md-6 mb-4" href="lista_usuarios.php">
+			<div class="card border-left-primary shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">admins</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['usuarios']; ?></div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-user fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+
+		<!-- Earnings (Monthly) Card Example -->
+		<a class="col-xl-3 col-md-6 mb-4" href="lista_cliente.php">
+			<div class="card border-left-success shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+					
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-success text-uppercase mb-1">Tecnicos</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['clientes']; ?></div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-users fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+		
+		<!-- Earnings (Monthly) Card Example -->
+
+		<a class="col-xl-3 col-md-6 mb-4" href="lista_productos.php">
+	
+			<div class="card border-left-info shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Productos</div>
+							<div class="row no-gutters align-items-center">
+								<div class="col-auto">
+									<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $data['productos']; ?></div>
+								</div>
+								<div class="col">
+									<div class="progress progress-sm mr-2">
+										<div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+
+		<!-- Pending Requests Card Example -->
+		
+			<a class="col-xl-3 col-md-6 mb-4" href="ventas.php">
+		
+			<div class="card border-left-warning shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pedidos</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['ventas']; ?></div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- Page Heading -->
+	
+		<h1 class="h3 mb-0 text-gray-800">Configuración</h1>
+	
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="card ">
+				<div class="card-header bg-primary text-white">
+					Información Personal
+				</div>
+				<div class="card-body"  >
+					<div class="form-group">
+						<label>Nombre: <strong><?php echo $_SESSION['nombre']; ?></strong></label>
+					</div>
+					<div class="form-group">
+						<label>Rol: <strong><?php echo $_SESSION['rol_name']; ?></strong></label>
+					</div>
+					<div class="form-group">
+						<label>Usuario: <strong><?php echo $_SESSION['user']; ?></strong></label>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+
+				</div>
+			</div>
+
+		
+	</div>
+</div>
+<!-- End of Main Content -->
