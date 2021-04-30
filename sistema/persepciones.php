97b2a33c8c70d3230823d6b4afe2244fa14db85a
Danilopez1702 <80540485+Danilgopez1702@users.noreply.github.com>
2021-04-30 16:17:00 -0500

    .

--- sistema/persepciones.php
+++ sistema/persepciones.php
@@ -1,1 +1,133 @@
-<?php include_once "include/header.php"; ?>
+<?php include_once "include/header.php";
+include "../conexion.php";
+if (!empty($_POST)) {
+    $alert = "";
+    if (empty($_POST['departamento']) ) {
+        $alert = '<div class="alert alert-danger" role="alert">
+                                    Todo los campos son obligatorio
+                                </div>';
+    } else {
+        $departamento = $_POST['departamento'];
+        $query_insert = mysqli_query($conexion, "INSERT INTO departamentos(nombre) values ('$departamento')");
+    }
+    mysqli_close($conexion);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+      Cargos
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Cargos</li>
+      </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+      <?php
+        if(isset($_SESSION['error'])){
+          echo "
+            <div class='alert alert-danger alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+              <h4><i class='icon fa fa-warning'></i> Error!</h4>
+              ".$_SESSION['error']."
+            </div>
+          ";
+          unset($_SESSION['error']);
+        }
+        if(isset($_SESSION['success'])){
+          echo "
+            <div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+              <h4><i class='icon fa fa-check'></i>¡Proceso Exitoso!</h4>
+              ".$_SESSION['success']."
+            </div>
+          ";
+          unset($_SESSION['success']);
+        }
+      ?>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <a href="#addnew" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> Nuevo</a>
+            </div>
+            <div class="box-body">
+              <table id="example1" class="table table-bordered">
+                <thead>
+                  <th>ID</th>
+                  <th>Descripcion</th>
+                  <th>Monto</th>
+                  <th>Acción</th>
+                </thead>
+                <tbody>
+                  <?php
+                    $query = mysqli_query($conexion, "SELECT * FROM percepciones");
+				    $result = mysqli_num_rows($query);
+                    if ($result > 0) {
+                    while ($row = mysqli_fetch_assoc($query)){
+                      echo "
+                        <tr>
+                          <td>".$row['id_per']."</td>
+                          <td>".$row['descripcion']."</td>
+                          <td>".number_format($row['monto'], 2)."</td>
+                          <td>
+                            <button class='btn btn-success btn-sm edit btn-flat' data-id='".$row['id_per']."'><i class='fa fa-edit'></i> Editar</button>
+                            <button class='btn btn-danger btn-sm delete btn-flat' data-id='".$row['id_per']."'><i class='fa fa-trash'></i> Eliminar</button>
+                          </td>
+                        </tr>
+                      ";
+                    }
+                    }?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>   
+  </div>
+</div>
+<script>
+$(function(){
+  $('.edit').click(function(e){
+    e.preventDefault();
+    $('#edit').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
+  });
+
+  $('.delete').click(function(e){
+    e.preventDefault();
+    $('#delete').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
+  });
+});
+
+function getRow(id){
+  $.ajax({
+    type: 'POST',
+    url: 'position_row.php',
+    data: {id:id},
+    dataType: 'json',
+    success: function(response){
+      $('#posid').val(response.id);
+      $('#edit_title').val(response.description);
+      $('#edit_rate').val(response.rate);
+      $('#del_posid').val(response.id);
+      $('#del_position').html(response.description);
+    }
+  });
+}
+</script>
+</body>
+</html>
+
