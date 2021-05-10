79f6e6c9042bb4eb41e121bc06e740b848850711
eed2e <eed.esteban@gmail.com>
2021-05-10 16:18:18 -0500

    Update add_empleado.php

--- sistema/add_empleado.php
+++ sistema/add_empleado.php
@@ -9,13 +9,14 @@ if (empty($_POST['id'] ) || empty($_POST['nombre'] )|| empty($_POST['ap'] )|| em
     $ap = $_POST['ap'];
     $am = $_POST['am'];
     $sm = $_POST['sm'];
+    $sd = $_POST['sd'];
     $fecha = $_POST['fecha'];
     $dep = $_POST['dep'];
     $status = 1;
 
    
     
-	    $sql = mysqli_query($conexion, "INSERT INTO empleados (id_empleado, nombre, apellido_paterno, apellido_materno, fecha_ingreso, sueldo_mensual, sueldo,diario, departamento, frecuencia_pago, status) 
+	    $sql = mysqli_query($conexion, "INSERT INTO empleados (id_empleado, nombre, apellido_paterno, apellido_materno, fecha_ingreso, sueldo_mensual, sueldo_diario, departamento, frecuencia_pago, status) 
         VALUES ('$id','$nombre','$ap','$am','$fecha','$sm','$sd','$dep','$fp','$status')");
 
         
