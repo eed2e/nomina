4d3b1f2bf1de3e32b8f717b4d0eb7a9856d441b0
Danilopez1702 <80540485+Danilgopez1702@users.noreply.github.com>
2021-05-17 18:59:32 -0500

    .

--- sistema/vacaciones.php
+++ sistema/vacaciones.php
@@ -109,7 +109,7 @@
                                 </div>
                             </div>
                         </div>
-                    </form>
+                    
                 </div>
 
                                   <!-- Modal 2 -->
